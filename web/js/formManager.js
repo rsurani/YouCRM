41334ef50fe888a640176634b35ddb1ea6593a24
Parth Dalvi <pdalvi@mindarraysystems.com>
2015-07-09 16:25:09 +0530

    update in comment

--- web/js/formManager.js
+++ web/js/formManager.js
@@ -4,81 +4,6 @@ var formManager = {
 
             var form =document.getElementById(context.container.attr('id'));
 
-/*
-   // var form =document.getElementById(context.container.attr('id')).nodeName;
-     //       var form =document.getElementById(context.container.attr('id'));
-          // alert(form.elements);
-       ///    alert(form.nodeName);
-          //  alert(form.elements.length);
-            //alert(context);
-
-	if (!form || form.nodeName!== "FORM") {
-            alert(form);
-            return;
-        }
-
-        var i, j, q = [];
-        for (i = form.elements.length - 1; i >= 0; i = i - 1) {
-                alert(i)
-                if (form.elements[i].name === "") {
-                        continue;
-                }
-                switch (form.elements[i].nodeName) {
-                case 'INPUT':
-                        switch (form.elements[i].type) {
-                        case 'text':
-
-                                alert("textbox");
-
-                        case 'hidden':
-                        case 'password':
-                                alert("password")
-                        case 'button':
-                        case 'reset':
-                        case 'submit':
-                                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                                break;
-                        case 'checkbox':
-                        case 'radio':
-                                if (form.elements[i].checked) {
-                                        q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                                }                                               
-                                break;
-                        }
-                        break;
-                        case 'file':
-                        break; 
-                case 'TEXTAREA':
-                        q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                        break;
-                case 'SELECT':
-                        switch (form.elements[i].type) {
-                        case 'select-one':
-                                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                                break;
-                        case 'select-multiple':
-                                for (j = form.elements[i].options.length - 1; j >= 0; j = j - 1) {
-                                        if (form.elements[i].options[j].selected) {
-                                                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].options[j].value));
-                                        }
-                                }
-                                break;
-                        }
-                        break;
-                case 'BUTTON':
-                        switch (form.elements[i].type) {
-                        case 'reset':
-                        case 'submit':
-                        case 'button':
-                                q.push(form.elements[i].name + "=" + encodeURIComponent(form.elements[i].value));
-                                break;
-                        }
-                        break;
-                }
-        }
-        //return q.join("&");
-
-*/
             context.params=context.params==undefined ? this.serializeForm(context.container): $.extend(context.params,this.serializeForm(context.container));
 
             //context.container=form.serialize();
@@ -103,8 +28,7 @@ var formManager = {
                                 element = $(elements[elementIndex]);
                                 if (!element.prop('disabled') && element.attr('name')) {
                                         if (element.attr('name')) {
-                                                alert(element.prop('tagName'));
-                                                alert(element.attr('name'));
+
 
                                                 params[element.attr('name')] = this.getElementValue(element);
                                         }
